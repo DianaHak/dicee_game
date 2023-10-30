e926e2b962b4d306e72a91abffae23100a2dc7ea
Diana Hakobian <dnhkbn@gmail.com>
2023-10-30 15:32:30 +0400

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ let randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
 function changeImage1() {
     if (randomNumber1 == 1){
-        document.getElementsByClassName("img1")[0].src = "images/dice1.png";
+        document.getElementsByClassName("img1")[0].src = "dice1.png";
     } else if(randomNumber1 == 2){
-        document.getElementsByClassName("img1")[0].src = "images/dice2.png";
+        document.getElementsByClassName("img1")[0].src = "dice2.png";
     } else if(randomNumber1 == 3){
-        document.getElementsByClassName("img1")[0].src = "images/dice3.png";
+        document.getElementsByClassName("img1")[0].src = "dice3.png";
     } else if(randomNumber1 == 4){
-        document.getElementsByClassName("img1")[0].src = "images/dice4.png";
+        document.getElementsByClassName("img1")[0].src = "dice4.png";
     } else if(randomNumber1 == 5){
-        document.getElementsByClassName("img1")[0].src = "images/dice5.png";
+        document.getElementsByClassName("img1")[0].src = "dice5.png";
     } else {
-        document.getElementsByClassName("img1")[0].src = "images/dice6.png";
+        document.getElementsByClassName("img1")[0].src = "dice6.png";
     }
 }
 
@@ -22,17 +22,17 @@ let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 function changeImage2() {
     if (randomNumber2 == 1){
-        document.getElementsByClassName("img2")[0].src = "images/dice1.png";
+        document.getElementsByClassName("img2")[0].src = "dice1.png";
     } else if(randomNumber2 == 2){
-        document.getElementsByClassName("img2")[0].src = "images/dice2.png";
+        document.getElementsByClassName("img2")[0].src = "dice2.png";
     } else if(randomNumber2 == 3){
-        document.getElementsByClassName("img2")[0].src = "images/dice3.png";
+        document.getElementsByClassName("img2")[0].src = "dice3.png";
     } else if(randomNumber2 == 4){
-        document.getElementsByClassName("img2")[0].src = "images/dice4.png";
+        document.getElementsByClassName("img2")[0].src = "dice4.png";
     } else if(randomNumber2 == 5){
-        document.getElementsByClassName("img2")[0].src = "images/dice5.png";
+        document.getElementsByClassName("img2")[0].src = "dice5.png";
     } else {
-        document.getElementsByClassName("img2")[0].src = "images/dice6.png";
+        document.getElementsByClassName("img2")[0].src = "dice6.png";
     }
 }
 
